4e938c2262efea6d7228b40a55eb77c4d5c9fcf7
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-08 00:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // write js code here if required
+// Get the current date and time
+let currentDate = new Date();
+
+// Get the HTML paragraph element
+let paragraph = document.querySelector('p');
+
+// Function to update the timer
+function updateTimer() {
+  // Get the current date and time
+  currentDate = new Date();
+
+  // Format the date and time as desired
+  let formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+  let formattedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+  // Display the timer on the HTML paragraph element
+  paragraph.textContent = `${formattedDate}, ${formattedTime}`;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Initial update of the timer
+updateTimer();
+
